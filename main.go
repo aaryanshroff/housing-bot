021201a057c6775ab38e995e9c0b776310a60661
Aaryan Shroff <unconfigured@null.spigotmc.org>
2022-06-30 15:39:56 -0400

    Trigger update of Kubernetes deployment

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main () {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
+        fmt.Fprintf(w, "Welcome to my Go bot!")
     })
 
     http.ListenAndServe(":80", nil)
